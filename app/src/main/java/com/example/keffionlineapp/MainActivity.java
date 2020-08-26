8c085ec95bfc9ceedb13e04faca3566b2237d4cf
Nyako Ephraim Alhamdu <kiddgit@gmail.com>
2020-08-26 15:41:39 -0800

    Did some UI redesign to the No Internet Dialog box

--- app/src/main/java/com/example/keffionlineapp/MainActivity.java
+++ app/src/main/java/com/example/keffionlineapp/MainActivity.java
@@ -1,14 +1,94 @@
 package com.example.keffionlineapp;
 
+import android.app.Dialog;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+// code written by @perfectephraim of Wullap Inc. 2020
+
+    private WebView myWebView;
+    public ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    // this is the main java method
+
+        progressBar  = (ProgressBar) findViewById(R.id.progressBar2);
+
+    // Start No internet connection dialog box
+        ConnectivityManager connectivityManager = (ConnectivityManager) getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        if (networkInfo==null || !networkInfo.isConnected() || !networkInfo.isAvailable()){
+            Dialog dialog = new Dialog(this);
+            dialog.setContentView(R.layout.alert_dialog);
+            dialog.setCancelable(false);
+            dialog.getWindow().setLayout(WindowManager.LayoutParams.WRAP_CONTENT,
+                    WindowManager.LayoutParams.WRAP_CONTENT);
+            dialog.getWindow().getAttributes().windowAnimations =
+                    android.R.style.Animation_Dialog;
+
+            Button button = dialog.findViewById(R.id.btn);
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    recreate();
+                }
+            });
+
+            dialog.show();
+
+        }
+        // END No internet connection dialog box
+
+        //call WebView method here
+            websiteView();
+
     }
+
+    //Method to start webview
+    private void websiteView(){
+
+        WebView myWebView = findViewById(R.id.webView);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        myWebView.loadUrl("https://keffionline.com");
+
+//        setting progressive bar on webview when loaded progressive bar disappears
+
+        myWebView.setWebViewClient(new WebViewClient(){
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+
+                progressBar.setVisibility(View.VISIBLE);
+                setTitle("Loading...");
+
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                progressBar.setVisibility(View.GONE);
+                setTitle(view.getTitle());
+            }
+        });
+
+    }//END webview method
+
+
+
 }
